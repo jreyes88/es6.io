3ace7739d17b7adc8af87f81bb17c5dffaa828bb
Joey Reyes <jreyes88@gmail.com>
2017-06-02 14:50:59 -0500

    Completed Unit 14

--- 14 - ES6 Tooling/02-babel/app-compiled.js
+++ 14 - ES6 Tooling/02-babel/app-compiled.js
@@ -2,17 +2,16 @@
 
 function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
 
-var _x$y$a$b = { x: 1, y: 2, a: 3, b: 4 };
-var x = _x$y$a$b.x;
-var y = _x$y$a$b.y;
-
-var z = _objectWithoutProperties(_x$y$a$b, ['x', 'y']);
+var _x$y$a$b = { x: 1, y: 2, a: 3, b: 4 },
+    x = _x$y$a$b.x,
+    y = _x$y$a$b.y,
+    z = _objectWithoutProperties(_x$y$a$b, ['x', 'y']);
 
 var age = 100;
-var people = ['Wes', 'Kait'];
+var people = ['Joey', 'Wes'];
 
 var fullNames = people.map(function (name) {
-  return name + ' Bos';
+  return '{name} Reyes';
 });
 
 var _iteratorNormalCompletion = true;
@@ -21,7 +20,7 @@ var _iteratorError = undefined;
 
 try {
   for (var _iterator = people[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-    var person = _step.value;
+    person = _step.value;
 
     console.log(person);
   }
